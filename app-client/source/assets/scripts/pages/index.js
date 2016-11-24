2cbf2b44aacdc1a0bb5e58e25350ce4e7f1e3ce6
wulechuan <wulechuan@live.com>
2016-11-24 16:33:41 +0800

    wip on swiper

--- app-client/source/assets/scripts/pages/index.js
+++ app-client/source/assets/scripts/pages/index.js
@@ -1,4 +1,11 @@
 (function () {
 	console.log($('.swiper-container.page-chief-sections')[0]);
-	var chiefPageContentSliders = new Swiper('.swiper-container.page-chief-sections');
+	var chiefPageContentSliders = new Swiper('.swiper-container.page-chief-sections', {
+		direction: 'vertical',
+		mousewheelControl: true,
+		hashnav: true,
+		loop: false,
+		pagination: '.swiper-pagination',
+		paginationClickable: true
+	});
 })();
