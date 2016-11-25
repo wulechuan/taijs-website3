3bd5b99afba0cfb6f5fef63fa05f3f37d3307f0b
wulechuan <wulechuan@live.com>
2016-11-25 13:29:02 +0800

    wip

--- app-client/source/assets/scripts/pages/index.js
+++ app-client/source/assets/scripts/pages/index.js
@@ -1,20 +1,34 @@
 (function () {
-	var chiefPageContentSliders = new Swiper('.swiper-container.page-chief-sections', {
-		direction: 'vertical',
-		mousewheelControl: true,
-		hashnav: true,
-		loop: false,
-		pagination: '.swiper-pagination',
-		paginationClickable: true,
+	var chiefPageContentSliders = new Swiper(
+		'.swiper-container.page-chief-sections',
+		{
+			direction: 'vertical',
+			mousewheelControl: true,
+			hashnav: true,
+			loop: false,
+			pagination: '.swiper-pagination',
+			paginationClickable: true,
 
-		onScroll: function(thisSwiper, event) {
-			console.log(thisSwiper.activeIndex, thisSwiper);
-			if (Math.random() > 0.5) {
-				thisSwiper.lockSwipes();
-			} else {
-				thisSwiper.unlockSwipes();
+			onScroll: function(thisSwiper, event) {
+				console.log(thisSwiper.activeIndex, thisSwiper);
+				if (Math.random() > 0.5) {
+					thisSwiper.lockSwipes();
+				} else {
+					thisSwiper.unlockSwipes();
+				}
 			}
 		}
-	});
-	chiefPageContentSliders.slideTo(chiefPageContentSliders.slides.length - 1, 5);
+	);
+
+	// by default, show the last slide at begining
+	var firstSlideToShow = chiefPageContentSliders.slides.length - 1; 
+
+	if (sessionStorage.getItem('lastShownSlide')) {
+		firstSlideToShow = sessionStorage.getItem('lastShownSlide');
+	}
+
+	chiefPageContentSliders.slideTo(
+		firstSlideToShow,
+		5 // milliseconds
+	);
 })();
