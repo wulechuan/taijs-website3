485c2830fbd2f34f2693406fd69bf1aa94d8ebef
wulechuan <wulechuan@live.com>
2016-11-25 13:00:52 +0800

    removed some useless css rules

--- app-client/source/assets/scripts/pages/index.js
+++ app-client/source/assets/scripts/pages/index.js
@@ -1,11 +1,20 @@
 (function () {
-	console.log($('.swiper-container.page-chief-sections')[0]);
 	var chiefPageContentSliders = new Swiper('.swiper-container.page-chief-sections', {
 		direction: 'vertical',
 		mousewheelControl: true,
 		hashnav: true,
 		loop: false,
 		pagination: '.swiper-pagination',
-		paginationClickable: true
+		paginationClickable: true,
+
+		onScroll: function(thisSwiper, event) {
+			console.log(thisSwiper.activeIndex, thisSwiper);
+			if (Math.random() > 0.5) {
+				thisSwiper.lockSwipes();
+			} else {
+				thisSwiper.unlockSwipes();
+			}
+		}
 	});
+	chiefPageContentSliders.slideTo(chiefPageContentSliders.slides.length - 1, 5);
 })();
